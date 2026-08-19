07ca9be342c6d5fdb7c714d46c283c48569c6ab9
Andrea Soto <sotoandrea585@gmail.com>
2026-08-18 22:55:40 -0400

    deleting old files

--- js/utils/product-utils.js
+++
@@ -1,5 +0,0 @@
-export const formatPrice = (value) => `Bs. ${Number(value).toFixed(2)}`;
-
-export function getProductImages(product) {
-  return product.images?.length ? product.images : [product.image];
-}
